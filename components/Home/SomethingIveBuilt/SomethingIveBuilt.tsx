bbcc160b72f5146d22a519a4a19ed4021757c4d8
suuraaj <suraaj,sinngh@gmail.com>
2025-03-08 18:56:11 +0530

    Initial commit

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -465,7 +465,147 @@ export default function SomethingIveBuilt() {
             </div>
           </div>
         </div>
+      
+
+    {/* // ? Project 7 */}
+    <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96">
+    {/* Left image */}
+    <div
+      className="hidden bg-AAprimary z-10  py-4 
+    absolute md:grid grid-cols-12 w-full h-full  content-center"
+    >
+      <div className="relative rounded w-full h-full col-span-7 ">
+        <a href="https://kanban-sooty-mu.vercel.app/" target={"_blank"} rel="noreferrer">
+          <div
+            // onClick={}
+            className="absolute w-full h-full rounded bg-AAprimary 
+     transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
+          ></div>
+        </a>
+        <Img src={"/kanban-ss.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
       </div>
     </div>
+
+    {/* right Content */}
+    <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+      {/* background for text in mobile responsive */}
+      <div className="absolute w-full h-full bg-opacity-70 z-0">
+        <div className="relative w-full h-full">
+          <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+          <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+          <Img src={"/kanban-ss.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
+        </div>
+      </div>
+      <div className="absolute w-full   rounded  ml-4 mt-2  text-end pr-8">
+        <div className="w-full"></div>
+        <span className="text-AAsecondary sm:text-lg bg-AAprimary px-2 py-1 rounded">In initiation...</span>
+      </div>
+      <div
+        className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+      col-span-8 flex flex-col items-start md:items-end sm:space-y-3 space-y-1"
+      >
+        <div className="flex flex-col space-y-1 md:items-end z-10">
+          <span className="text-AAsecondary text-base">Recent Project</span>
+          <a href="https://kanban-sooty-mu.vercel.app/" target={"_blank"} rel="noreferrer">
+            <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                Kanban
+            </span>
+          </a>
+        </div>
+        <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+          <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+          Kanban is a modern <span></span>
+           <span className="text-AAsecondary">task management website designed to streamline your workflow</span><span></span>,It provides an intuitive and visually appealing way to organize and track tasks efficiently, 
+           helping you stay productive and focused. 🚀
+          </p>
+        </div>
+        <ul
+          className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+         text-sm font-Text2 md:justify-end"
+        >
+          <span className="pr-4 z-10">ReactJs</span>
+          <span className="pr-4 z-10">TailwindCSS</span>
+          <span className="pr-4 z-10">HTML</span>
+          <span className="pr-4 z-10">Typescript</span>
+        </ul>
+        <div className="z-10 flex fle-row space-x-5 sm:pt-0 pt-2">
+          <GithubIcon link="https://github.com/iamsurajchahar/kanban" />
+          <a href="https://kanban-sooty-mu.vercel.app/" target={"_blank"} rel="noreferrer">
+            <ExternalLink url={""} router={router} />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+            {/* // ?  Project 6 */}
+         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center "
+          >
+            <div className="relative rounded w-full h-full col-start-6 col-span-7 ">
+              <a href="https://github.com/iamsurajchahar/Time-series-data-visualizer" target={"_blank"} rel="noreferrer">
+                <div
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-60 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <Img src={"/time-ss.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0 md:order-2">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAprimary opacity-50 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/time-ss.png"} alt={"Project Screen shot"} className={`w-full h-full`} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6   
+            col-span-8 flex flex-col items-start  sm:space-y-3 md:order-1"
+            >
+              <div className="flex flex-col space-y-1  z-10">
+                <span className="text-AAsecondary text-base">Recent Project</span>
+                <a href="https://github.com/iamsurajchahar/Time-series-data-visualizer" target={"_blank"} rel="noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                  A Smarter Way to Visualize JSON Data 
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left ">
+                Effortlessly explore and analyze JSON data with JSON Tree, a powerful visualization tool 
+                designed for clarity and efficiency. Whether you're a developer, data analyst, or API enthusiast, 
+                JSON Tree simplifies data representation in both graph 📈 and tree 🌲 formats, 
+                making it easier to navigate complex structures.
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-start"
+              >
+                <span className="pr-4 z-10">NextJs</span>             
+                <span className="pr-4 z-10">TailwindCSS</span>
+                <span className="pr-4 z-10">Tauri</span>
+                <span className="pr-4 z-10">typescript</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 sm:pt-0 pt-2">
+                <GithubIcon link="https://github.com/iamsurajchahar/Time-series-data-visualizer" />
+                <a href="https://github.com/iamsurajchahar/Time-series-data-visualizer" target={"_blank"} rel="noreferrer">
+                  <ExternalLink url={""} router={router} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+  </div>
+  </div>
   );
 }
